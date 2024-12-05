09b107beae986a7c30d52d91af46c78e5c960027
abdelahmed <abdel.ahmed.2002@gmail.com>
2024-12-05 11:19:00 -0800

    m3 pass

--- lib/grading/tests/test_m3.c
+++ lib/grading/tests/test_m3.c
@@ -281,6 +281,7 @@ errval_t grading_run_tests_processes(void)
     grading_printf("# TESTS: Milestone 3 (Process Management)        \n");
     grading_printf("#################################################\n");
     
+    spawn_one_with_default_args();
     spawn_one_without_args();
     spawn_one_with_default_args();
     spawn_one_with_args();
